e5f76439c920ed040273cae8c660eb4de10c2d86
Mahruf Hossain <mahrufhossain97@gmail.com>
2024-06-01 23:30:21 +0600

    Updated the factorial.c code

---
+++ practice_C/practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+long long int factorial(int n){
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main(void){
+    int number;
+    printf("Enter the number: \n");
+    scanf("%d", &number);
+
+    printf("The factorial of the given number is %lld", factorial(number));
+}
